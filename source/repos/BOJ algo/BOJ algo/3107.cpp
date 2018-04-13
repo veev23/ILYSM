ad7d03febfcd92a7b51186353dc38b502fdd490c
veev23 <veev23@gmail.com>
2018-04-13 23:11:04 +0900

    예외처리

---
+++ source/repos/BOJ algo/BOJ algo/3107.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie();
+	char input[40];
+	string str[8];
+	cin >> input;
+	int k = 0;
+	int i = 0;
+	while(true){
+		while (input[k] != ':') {
+			if (input[k]=='\0') break;
+			str[i] += input[k++];
+		}
+		if (input[k] == ':') i++;
+		if (input[k] == '\0') break;
+		k++;
+	}
+	int remember = -1; //::ÀÇ À§Ä¡
+	int count = 0;
+	for (int i = 0; i < 8; i++) {
+		if (str[i].length()==0) {
+			count++;
+			if (remember == -1) remember = i;
+		}
+		else
+			while(str[i].length() < 4) {
+				str[i] = '0' + str[i];
+			}
+	}
+	if (count == 0) count++;
+	for (int i = 0; i < 9-count; i++) {
+		if (i == remember && count > 0) {
+			for (int j = 0; j < count; j++) {
+				cout << "0000";
+				if (i == 8 - count && j == count - 1) break;
+				cout << ':';
+			}
+		}
+		else cout << str[i];
+		if (i == 8-count) continue;
+		if(i!=remember)
+			cout << ':';
+	}
+	cout << '\n';
+	return 0;
+}
